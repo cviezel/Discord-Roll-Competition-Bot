f8dcd57682667e1b82be41231b126259e915f416
cviezel <cviezel@gmail.com>
2017-12-19 11:45:03 -0500

    created roller bot

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-var first = 0;
+var count = 0;
 var user_array = [];
 var roll_array = [];
 var num;
+var cheatflag = 0;
 
 bot.on('message', function(message) {
     // Don't forget to log the message!
     var mes = message.content.split(" ");
-    var person = message.author.username;
     if(mes == '!enter')
     {
-        message.channel.send(person + ' is rolling');
-        num = Math.floor((Math.random() * 100) + 1);
-        user_array.push(person);
-        roll_array.push(num);
+        cheatflag = 0;
+        var person = message.author.username;
+        for (var i = 0; i < user_array.length; i++){
+            if(person == user_array[i]) {
+                message.reply('You already entered!');
+                cheatflag = 1;
+                break;
+                }
+        }
+        if (cheatflag == 0)
+        {
+            message.reply('You have been entered!');
+            count++;
+            num = Math.floor((Math.random() * 1000) + 1);
+            user_array.push(person);
+            roll_array.push(num);
+        }
     }
     if(mes == '!roll')
     {
-        message.channel.send(user_array[0] + ' rolled a ' + roll_array[0]);
+        if (count == 0)
+        {
+            message.channel.send('Nobody has entered yet!');
+        }
+        else {
+        message.channel.send('3');
+        message.channel.send('2');
+        message.channel.send('1');
+        for (var i = 0; i < roll_array.length; i++) {
+            message.channel.send(user_array[i] + ' rolled ' + roll_array[i]);
+        }
+        var max = 0;
+        for (var i = 0; i < roll_array.length; i++){
+            if(roll_array[i] > roll_array[max]) {
+                    max = i;
+                }
+        }
+        message.channel.send('------------------------------');
+        message.channel.send(user_array[max] + ' won with ' + roll_array[max] + '!');
+        user_array.splice(0,user_array.length);
+        roll_array.splice(0,roll_array.length);
+        count = 0;
+    }
+}
+    if (mes == '!clear')
+    {
+        message.channel.send('All entries have been cleared.');
+        user_array.splice(0,user_array.length);
+        roll_array.splice(0,roll_array.length);
+        count = 0;
     }
 });
 
